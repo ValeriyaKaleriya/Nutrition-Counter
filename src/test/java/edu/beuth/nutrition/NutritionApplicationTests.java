f91eab6eb5a881660f0a7acc33e692dde6f9c5ec
ValeriyaKaleriya <andrushchakval@gmail.com>
2019-02-01 17:40:39 +0100

    init

---
+++ src/test/java/edu/beuth/nutrition/NutritionApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.beuth.nutrition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NutritionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
