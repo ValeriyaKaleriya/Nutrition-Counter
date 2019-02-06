a9a7af2153bb1b9cab4d05e07dfe89609a8bd74d
ValeriyaKaleriya <ValeriyaKaleriya>
2019-02-06 21:31:54 +0100

    Setup the spring web application

---
+++ src/main/java/edu/beuth/nutrition/pages/index/IndexController.java
@@ -0,0 +1,13 @@
+package edu.beuth.nutrition.pages.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    String index() {
+        return "index";
+    }
+}
